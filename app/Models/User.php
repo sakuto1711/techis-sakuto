b2ae35efb415f931c73b83f4887bba2a91d080aa
sakuto1711 <179001448+sakuto1711@users.noreply.github.com>
2025-01-31 20:21:13 +0900

    権限

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function isAdmin() {
+            return $this->role === 'admin'; 
+        }
 }
